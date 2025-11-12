1ca22fc9f6d7c267209487e7aaf48f4c782edd34
Artem Podorozhko <artem.podorozhko@outlook.com>
2025-11-12 10:24:30 +0200

    SS.Notifier project

---
+++ SS.Notifier/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SS.Notifier;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        await host.RunAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                config.AddUserSecrets<Program>(); // Loads user secrets
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHttpClient();
+            });
+}
